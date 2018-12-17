bbc554d653cee165c074f3b8f192bfa5abdaa20f
Russell Koons <rkoons@gmail.com>
2018-12-17 16:14:17 -0600

    added tests and travis

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+let server;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
+module.exports = { app };
---
+++ test/test-server.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const { app, runServer, closeServer } = require("../server");
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Testing the server", function() {
+
+  it("should respond", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
